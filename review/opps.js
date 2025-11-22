7376038afdd1eed76e70e3a018f970b8047aab3a
thasnimpk <fathimathasnim220@gmail.com>
2025-11-22 22:25:55 +0530

    first commit

---
+++ review/opps.js
@@ -0,0 +1,12 @@
+class a{
+    constructor(){
+        this.a=20
+        this.b=30
+    }
+    sum(){
+        return this.a+this.b
+    }
+}
+
+let obj=new a()
+console.log(obj.new());
---
+++ review/rv.js
@@ -0,0 +1,71 @@
+// let n = 15;
+// let a = 0, b = 1;
+
+// const { useState } = require("react");
+
+// console.log(a); 
+// console.log(b); 
+
+// for (let i = 2; i < n; i++) {
+//   let c = a + b;
+//   console.log(c);
+//   a = b;
+//   b = c;
+// } 
+
+
+// let a=[0,1] 
+// let i=0
+// while(i<10){
+//      a.push (a[i]+a[i+1]); 
+//  i++
+//  }
+// console. log(a)
+
+
+
+// let a=[1,2,3,4,5,6,7,8,9,10]
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+// let a= "malayalkam";
+// let reversed= a.split('').reverse().join('');
+// if(a===reversed){
+//     console.log("Palindrome");
+// } else{
+//     console.log("Not Palindrome");
+// }
+ 
+// let a= 1;
+// let array =[];
+// for (let i = 0;i<5;i++){
+//   array.push(a);
+//   a+=3;       
+// }
+// console.log(array); 
+
+ 
+ 
+let a=[1,2,3,4,5,6]
+b=a.filter ((items)=>{
+return items<=5
+})
+console.log(b);
+
+
+
+const[count,setCount]=useState(0)
+  
+
+
+function count (count){
+
+}
